19e8125bd896b815bb5d3d123a4acdf3f72c8fc2
davidlucid <davidlucidsl@gmail.com>
2020-10-23 13:48:25 -0500

    Fixed scripts/ganche.js overwriting DEVELOPMENT_WEB3_PROVIDER_URL_TO_BE_FORKED

--- scripts/ganache.js
+++ scripts/ganache.js
@@ -7,11 +7,12 @@ const fs = require('fs');
 server.listen(8546, function(err, blockchain) {
     if (err) return console.log(err);
 
-    let keyVars = 'DEVELOPMENT_ADDRESS=\"' + Object.keys(blockchain['unlocked_accounts'])[0] + "\"\n";
-    keyVars += 'DEVELOPMENT_ADDRESS_SECONDARY=\"' + Object.keys(blockchain['unlocked_accounts'])[1] + "\"\n";
+    let keyVars = "DEVELOPMENT_WEB3_PROVIDER_URL_TO_BE_FORKED=\"" + process.env.DEVELOPMENT_WEB3_PROVIDER_URL_TO_BE_FORKED + "\"\n";
+    keyVars += "DEVELOPMENT_ADDRESS=\"" + Object.keys(blockchain['unlocked_accounts'])[0] + "\"\n";
+    keyVars += "DEVELOPMENT_ADDRESS_SECONDARY=\"" + Object.keys(blockchain['unlocked_accounts'])[1] + "\"\n";
 
     fs.writeFile('.env', keyVars, function(err) {
-    	if(err) console.log(err);
+    	if (err) console.log(err);
     	console.log('wrote keys to .env with no errors');
     });
 });
